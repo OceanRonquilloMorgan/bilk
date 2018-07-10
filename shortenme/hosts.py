e4785a6cde2af08f50bee774102e079e706d68f3
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-07-10 17:34:07 +0200

    Fix redirect????

--- shortenme/hosts.py
+++ shortenme/hosts.py
@@ -2,6 +2,6 @@ from django.conf import settings
 from django_hosts import patterns, host
 
 host_patterns = patterns('',
-    # host(r'www', settings.ROOT_URLCONF, name='www'),
+    host(r'bilk.herokuapp.com', settings.ROOT_URLCONF, name='bilk.herokuapp.com'),
     # host(r'(?!www).*', 'shortenme.hostsconf.urls', name='wildcard'),
 )
