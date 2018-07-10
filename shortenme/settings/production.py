1c89adb8e99f1b494025eaa97ece5c8b7a7fc658
Ocean Ronquillo-Morgan <oronquil@usc.edu>
2018-07-10 16:33:18 +0200

    Debugger off

--- shortenme/settings/production.py
+++ shortenme/settings/production.py
@@ -21,10 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['bilk.herokuapp.com']
-
 
 # Application definition
 
